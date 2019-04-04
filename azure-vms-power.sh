a40bc3cc580edf6b2cba1bde270292aec41fbc42
cristiano-degiorgis <cri@webprofessor.it>
2019-04-04 23:49:27 +0200

    vm start

--- azure-vms-power.sh
+++ azure-vms-power.sh
@@ -6,16 +6,21 @@ fi
 
 RESOURCE_GROUP=$2
 
+vms_ids=$(az vm list -g "$RESOURCE_GROUP" --query "[].id" -o tsv)
+echo $vms_ids
+
 if [ $1 = 'stop' ]
   then
     echo "Stopping VMs"
-
-    vms_ids=$(az vm list -g "$RESOURCE_GROUP" --query "[].id" -o tsv)
-
-    echo $vms_ids
-
     az vm stop --ids $vms_ids
     az vm deallocate --ids $vms_ids
+fi
+
+if [ $1 = 'start' ]
+  then
+    echo "Restarting VMs"
+
+    az vm start --ids $vms_ids
 fi
 
 az vm show -d --ids $vms_ids | grep powerState
